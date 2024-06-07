ab5729423e73a9b522aeea06e957a0e26f787ed3
fatemeh <babaei.f1996@gmail.com>
2024-06-07 23:09:36 +0330

    style: add style for time

--- frontend/public/js/app.js
+++ frontend/public/js/app.js
@@ -15,7 +15,6 @@ const socket = io("http://localhost:4001", {
 const input = document.querySelector(".message-box");
 const chat = document.querySelector(".messages-chat");
 
-
 socket.on("connect", () => {
     input.addEventListener("keydown", (event) => {
         if (event.keyCode === 13) {
@@ -34,11 +33,13 @@ socket.on("connect", () => {
                     ? `<div class="message">
                         <div class="response">
                             <p class="text">${data.message}</p>
-                            <p>${new Date().getHours()} : ${new Date().getMinutes()} :${new Date().getSeconds()} </p>
+                            <p class="time">${new Date().getHours()} : ${new Date().getMinutes()} :${new Date().getSeconds()} </p>
                         </div>
                     </div>`
                     : `<div class="message">
-                    <p class="text">${data.message }</p>  <p>${new Date().getHours()} : ${new Date().getMinutes()} :${new Date().getSeconds()} </p></div>`
+                        <p class="text">${data.message}</p>
+                        </div>
+                        <p class="time">${new Date().getHours()} : ${new Date().getMinutes()} :${new Date().getSeconds()} </p>`
             }`
         );
     });
